f7359926cfc3eb709d5381f0e9d6c2aa0f85c7d2
harshvardhanb25 <harshvardhanb25@arizona.edu>
2026-02-25 21:12:08 +0530

    create config.py

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import os
+
+# Base directory relative path
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+RAW_DATA_DIR = os.path.join(DATA_DIR, "raw")
+
+# File Paths
+NSE_EQ_PATH = os.path.join(RAW_DATA_DIR, "nse_eq_master.csv")
+NSE_ETF_PATH = os.path.join(RAW_DATA_DIR, "nse_etf_master.csv")
+SGB_PATH = os.path.join(RAW_DATA_DIR, "SGBMAY28.csv")
+PORTFOLIO_PATH = os.path.join(RAW_DATA_DIR, "current_portfolio.csv")
