79ec3e20618385a5b56a2b5105977fff3bf34f18
harshvardhanb25 <harshvardhanb25@arizona.edu>
2026-03-02 23:45:06 +0530

    Add function to compute individual annualized volatility

--- src/utils/features.py
+++ src/utils/features.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def compute_daily_ret(price_history: pd.DataFrame) -> pd.DataFrame:
@@ -7,6 +8,10 @@ def compute_daily_ret(price_history: pd.DataFrame) -> pd.DataFrame:
 
 def compute_annualized_mean_ret(ret: pd.DataFrame) -> pd.Series:
     return ret.mean() * 252
+
+
+def compute_individual_annualized_volatility(ret: pd.DataFrame) -> pd.Series:
+    return ret.std() * np.sqrt(252)
 
 
 # TODO: Implement cov matrix logic
