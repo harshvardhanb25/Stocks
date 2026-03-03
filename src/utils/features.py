bd83c1e183c3c35d2e7e9795f26dbe5218e20871
harshvardhanb25 <harshvardhanb25@arizona.edu>
2026-03-03 23:54:57 +0530

    Minor comment fixes

--- src/utils/features.py
+++ src/utils/features.py
@@ -3,14 +3,17 @@ import numpy as np
 
 
 def compute_daily_ret(price_history: pd.DataFrame) -> pd.DataFrame:
+    """Compute daily returns for each security independently"""
     return price_history.pct_change().dropna(how="all")
 
 
 def compute_annualized_mean_ret(ret: pd.DataFrame) -> pd.Series:
+    """ "Compute annualized mean return for each security independently"""
     return ret.mean() * 252
 
 
 def compute_individual_annualized_volatility(ret: pd.DataFrame) -> pd.Series:
+    """Compute annualized volatility for each security independently"""
     return ret.std() * np.sqrt(252)
 
 
