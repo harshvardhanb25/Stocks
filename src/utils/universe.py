6c0fb28d02275fa2207266c8e9933581111b9d03
harshvardhanb25 <harshvardhanb25@arizona.edu>
2026-03-07 00:06:21 +0530

    implement function to remove securities from universe

--- src/utils/universe.py
+++ src/utils/universe.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def _remove_from_canon(
     canon_df: pd.DataFrame,
-    symbols: str | list[str],
+    symbols: list[str],
 ) -> pd.DataFrame:
     return canon_df[~canon_df["symbol"].isin(symbols)]
 
@@ -13,3 +13,16 @@ def _remove_from_returns(
     symbols: list[str],
 ) -> pd.DataFrame:
     return ret.drop(columns=[s for s in ret if s in symbols])
+
+
+def remove_securities(
+    canon_df: pd.DataFrame, ret: pd.DataFrame, symbols: str | list[str]
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+
+    if isinstance(symbols, str):
+        symbols = [symbols]
+
+    return (
+        _remove_from_canon(canon_df=canon_df, symbols=symbols),
+        _remove_from_returns(ret=ret, symbols=symbols),
+    )
